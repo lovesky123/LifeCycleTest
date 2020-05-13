54bf89ea7def7d50a178799551c4e0b8b42fb6a6
楚留香 <1440355743@qq.com>
2020-05-13 21:23:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lifecycletest/test/MyViewModel.java
@@ -0,0 +1,18 @@
+package com.example.lifecycletest.test;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+/**
+ * @Date 2019/10/30 0030
+ **/
+public class MyViewModel extends AndroidViewModel {
+
+    public MyViewModel(@NonNull Application application) {
+        super(application);
+    }
+
+    //...
+}
